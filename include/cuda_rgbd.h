e03ef8864ee697045c2c25fdece2ca1cbf8c3fa1
Carlos Tojal <carlos.tojal@hotmail.com>
2023-04-11 18:25:11 +0100

    fix library includes and cmakelists

--- include/cuda_rgbd.h
+++ include/cuda_rgbd.h
@@ -2,6 +2,7 @@
 #define CUDA_RGBD_H_
 
 #include <cuda_runtime.h>
+#include <pcl/point_types.h>
 
 __global__ void deprojectImageKernel(std::uint8_t* depth, std::uint8_t* color, pcl::PointXYZRGB* cloud, int width, int height, float fx, float fy, float cx, float cy);
 
